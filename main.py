e66fdf3c14f07d0d151dffcbf9d290174670b45f
unknown <sean.herbison@gmail.com>
2023-10-03 19:27:28 -0700

    Finished even_or_odd function.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-def even_or_odd():
+# Time complexity O(1)
+def even_or_odd(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+
+def less_than_100(number_list):
     pass
 
-def less_than_100():
+def repeated_names(name_list):
     pass
 
-def repeated_names():
+def sort_list(number_list):
     pass
 
-def sort_list():
-    pass
+# Even or Odd Tests
+print(even_or_odd(2)) # True
+print(even_or_odd(3)) # False
+print(even_or_odd(4)) # True
+print(even_or_odd(5.5)) # False, because not designed for floats
