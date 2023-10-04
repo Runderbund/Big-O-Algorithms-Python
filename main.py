0f317ad1ede05c3d55f8bf76bf042d4c9c533722
unknown <sean.herbison@gmail.com>
2023-10-03 19:29:22 -0700

    Finished less_than_100 function.

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def even_or_odd(number):
     return False
 
 
+# Time complexity O(n)
 def less_than_100(number_list):
-    pass
+    for n in number_list:
+        if n > 100:
+            return False
+    return True
 
 def repeated_names(name_list):
     pass
@@ -19,3 +23,8 @@ print(even_or_odd(2)) # True
 print(even_or_odd(3)) # False
 print(even_or_odd(4)) # True
 print(even_or_odd(5.5)) # False, because not designed for floats
+
+# Even or Odd Tests
+print(less_than_100([2, 3, 4, 5])) # True
+print(less_than_100([200, 3, 4, 5])) # False
+print(less_than_100([2, 4, 98, 101])) # False
